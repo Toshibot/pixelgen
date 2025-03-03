c7d1c87df1df3ce42f8279db2ed8e235fdaf847f
toshibot <toshi.stewart@gmail.com>
2025-03-03 13:43:28 +1100

    Meta description

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 <template>
   <body id="Body" class="t-dark">
     <main class="o-main u-vert-center">
+      <div class="logo"><span class="pixel">pixel</span>gen<span class="dot">.</span>au</div>
+      <div>coming soon</div>
     </main>
   </body>
 </template>
@@ -21,6 +23,35 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-
+  body {
+    overflow: hidden;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .logo {
+    font-family: "Jersey 10", serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 64px;
+    text-align: center;
+    color: #999;
+    .pixel {
+      color: #fff;
+    }
+    .dot {
+      color: #F33464;
+    }
+  }
 </style>
 
